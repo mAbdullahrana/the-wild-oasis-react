e5d88a422b012e25af35b4bbf8e7e1aae6f77821
mmAbdullahrana <ar292211@gmail.com>
2024-11-23 16:51:37 +0500

    Resloved checkin error

--- src/App.jsx
+++ src/App.jsx
@@ -147,9 +147,9 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeToggleProvider } from "./context/DarkModeToggle.jsx";
+import CheckIn from "./pages/CheckIn.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -180,7 +180,7 @@ function App() {
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
+              <Route path="checkin/:bookingId" element={<CheckIn />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
               <Route path="settings" element={<Settings />} />
